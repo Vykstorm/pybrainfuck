7868bbfffe37f175b228aed45ee48cc44e03d80d
Vykstorm <victorruizgomezdev@gmail.com>
2019-08-21 13:27:26 +0200

    Added script to execute brainfuck code interactively

---
+++ src/__main__.py
@@ -0,0 +1,10 @@
+
+'''
+This script runs an interactive brainfuck console.
+'''
+
+from src import BrainfuckConsole
+
+
+if __name__ == '__main__':
+	BrainfuckConsole().interact(banner='', exitmsg='')
